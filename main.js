0ea0b5ccd3b65b86a9606ab1a0af06fb422c3f93
Nduka Henry <henrynduka17@gmail.com>
2022-08-10 02:33:34 -0700

    added
    showcase

---
+++ main.js
@@ -0,0 +1,20 @@
+function box1Function(){
+    document.getElementById("displayed-img").src = "./Images/Item11/5.jpg";
+}
+
+function box2Function(){
+    document.getElementById("displayed-img").src = "./Images/Item11/6.jpg";
+}
+
+function box3Function(){
+    document.getElementById("displayed-img").src = "./Images/Item11/7.jpg";
+}
+
+function box4Function(){
+    document.getElementById("displayed-img").src = "./Images/Item11/8.jpg";
+}
+document.getElementById("test").innerHTML = "buying and selling";
+
+function myfunction(){
+    document.getElementById("test").style.backgroundColor:
+}
